a2ca72ed1fa29965588ca2f009baa289b181e752
howardjake <howardjake9512@gmail.com>
2023-06-22 14:03:56 -0400

    add leet code link

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,14 +5,24 @@ const Blog = (props) => {
         <div className="blog">
             
             <div>
-            <a href="https://github.com/howardjake/My_Coding_Dairy">
+            <a target="_blank" rel="noreferrer" href="https://github.com/howardjake/leet-coder">
+                <img  className="blog-img" src="projects/leet-code.png" alt="" />
+            </a>
+
+            <h2 className="blog-message">This is where I have been keeping track of my LeetCode progress.
+            <br />
+            <a target="_blank" rel="noreferrer" href="https://github.com/howardjake/leet-coder"> <i className="fas fa-arrow-circle-right"></i>leet-coder</a></h2>
+            </div>
+
+            <div>
+            <a target="_blank" rel="noreferrer" href="https://github.com/howardjake/My_Coding_Dairy">
                 <img  className="blog-img" src="projects/Blog.png" alt="" />
             </a>
-            
-            </div>
-            <h2 className="blog-message">I record my code challenge journey in this repository. Follow along if you'd like to challenge yourself too. 
+
+            <h2 className="blog-message">In the past I recorded my code challenge journey in this repository.
             <br />
-            <a href="https://github.com/howardjake/My_Coding_Dairy"> <i className="fas fa-arrow-circle-right"></i>My Coding Diary</a></h2>
+            <a target="_blank" rel="noreferrer" href="https://github.com/howardjake/My_Coding_Dairy"> <i className="fas fa-arrow-circle-right"></i>My Coding Diary</a></h2>
+            </div>
             
         </div>
     )
